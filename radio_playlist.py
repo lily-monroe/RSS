1b989084e889f6e85bd6d374ace1a60b85d0ac0f
lily-monroe <kgurbiel@gmail.com>
2025-05-20 21:40:10 +0200

    Update radio_playlist.py

--- radio_playlist.py
+++ radio_playlist.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import re
 import xml.etree.ElementTree as ET
+from urllib.parse import quote_plus
 
 def pobierz_strone(url):
     """Pobiera zawartość strony HTML."""
@@ -17,90 +18,90 @@ def pobierz_strone(url):
         print(f"Wystąpił błąd podczas pobierania strony {url}: {e}")
         return None
 
-def parsuj_myradioonline(html_content, stacja_nazwa):
-    """Parsuje listę utworów ze strony myradioonline.pl."""
+def parsuj_myradioonline(html_content, stacja_nazwa, stacja_url):
+    """Parsuje listę utworów ze strony myradioonline.pl/playlista."""
     soup = BeautifulSoup(html_content, 'html.parser')
-    utwory = soup.find_all('div', class_='songCont')
+    # Szukamy divów o klasie 'yt-row' z itemprop="track"
+    utwory_html = soup.find_all('div', itemprop='track', class_='yt-row')
     lista_parsowanych_utworow = []
 
-    for utwor in utwory:
-        txt1_div = utwor.find('div', class_='txt1 anim')
+    for utwor_html in utwory_html:
+        # {%1} - data-youtube
+        youtube_id = utwor_html.find('div', class_=re.compile(r'txt1 anim'))
+        youtube_id = youtube_id.get('data-youtube', '') if youtube_id else ''
         
-        czas_emisji_pelny = ''
-        tytul_wykonawca_raw = ''
+        # {%2} - data i czas odtworzenia (z span class="txt2 mcolumn" data-original-title)
+        czas_emisji_span = utwor_html.find('span', class_='txt2 mcolumn')
+        czas_emisji_pelny = czas_emisji_span.get('data-original-title', '').strip() if czas_emisji_span else ''
 
-        if txt1_div:
-            span_czas = txt1_div.find('span', class_='txt2')
-            if span_czas:
-                czas_emisji_pelny = span_czas.get('title', '').strip()
-                tytul_wykonawca_raw = txt1_div.text.replace(czas_emisji_pelny, '').strip()
-            else:
-                tytul_wykonawca_raw = txt1_div.text.strip()
+        # {%3} - nazwa artysty (span itemprop="byArtist")
+        wykonawca_span = utwor_html.find('span', itemprop='byArtist')
+        wykonawca = wykonawca_span.text.strip() if wykonawca_span else ''
 
-        wykonawca = ''
-        tytul = ''
+        # {%4} - tytuł utworu (span itemprop="name")
+        tytul_span = utwor_html.find('span', itemprop='name')
+        tytul = tytul_span.text.strip() if tytul_span else ''
 
-        if ' - ' in tytul_wykonawca_raw:
-            parts = tytul_wykonawca_raw.split(' - ', 1)
-            wykonawca = re.sub(r'^\d{2}:\d{2}', '', parts[0]).strip()
-            tytul = parts[1].strip()
-        else:
-            tytul = tytul_wykonawca_raw.strip()
-            tytul = re.sub(r'^\d{2}:\d{2}', '', tytul).strip()
-            wykonawca = '' 
-
-        czas_emisji = ''
-        data_emisji = ''
-        if czas_emisji_pelny:
-            parts_czas = czas_emisji_pelny.split(' ')
-            if len(parts_czas) == 2:
-                data_emisji = parts_czas[0]
-                czas_emisji = parts_czas[1]
-
-        image_span = utwor.find('span', class_='cont anim')
-        image_url = image_span.find('img', class_='js-img-lload').get('data-lazy-load', '') if image_span and image_span.find('img', class_='js-img-lload') else ''
+        # Adres zdjęcia (z span class="cont anim" > img data-lazy-load)
+        # To jest ten sam obrazek co wcześniej, tylko teraz szukamy go w kontekście całego utworu
+        image_span = utwor_html.find_previous_sibling('div', class_='songCont') # Sprawdź poprzedni sibling, jeśli struktura się zmieniła
+        if not image_span: # Jeśli nie ma, szukamy w całym rodzicu
+            image_span = utwor_html.find_parent('div', class_='songCont')
+        
+        image_url = ''
+        if image_span:
+            img_tag = image_span.find('img', class_='js-img-lload')
+            image_url = img_tag.get('data-lazy-load', '') if img_tag else ''
+        
         image_url_big = image_url.replace('50x50bb.webp', '1000x1000bb.webp') if image_url else ''
 
-        youtube_id = utwor.get('data-youtube', '')
-        # Używamy http://youtube.com/watch?v=... jako linku do YouTube
-        youtube_link = f'http://youtube.com/watch?v={youtube_id}' if youtube_id else ''
+        # Jeśli brakuje kluczowych danych, pomijamy utwór
+        if not (wykonawca and tytul and czas_emisji_pelny):
+            continue
 
         lista_parsowanych_utworow.append({
             'stacja': stacja_nazwa,
+            'stacja_url': stacja_url, # Dodajemy URL stacji do linku RSS
             'wykonawca': wykonawca,
             'tytul': tytul,
-            'data_emisji': data_emisji,
-            'czas_emisji': czas_emisji,
+            'czas_emisji_pelny': czas_emisji_pelny, # Pełny czas emisji
             'image_url_big': image_url_big,
-            'youtube_link': youtube_link
+            'youtube_id': youtube_id # ID YouTube
         })
     return lista_parsowanych_utworow
 
-def parsuj_ukradiolive(html_content, stacja_nazwa):
-    """Parsuje listę utworów ze strony ukradiolive.com."""
+def parsuj_ukradiolive(html_content, stacja_nazwa, stacja_url):
+    """Parsuje listę utworów ze strony ukradiolive.com/playlist."""
     soup = BeautifulSoup(html_content, 'html.parser')
     lista_parsowanych_utworow = []
 
-    playlist_items = soup.select('div.col-md-9 ul.list-group li.list-group-item')
+    # Struktura dla ukradiolive.com/playlist jest inna niż dla /bbc-radio-1/
+    # Niestety nie ma tu tak rozbudowanych danych jak na myradioonline.pl (brak youtube_id, obrazków)
+    playlist_items = soup.select('div.col-md-9 ul.list-group li.list-group-item') # Potencjalnie może być 'li.list-group-item'
 
     for item in playlist_items:
         text_content = item.get_text(strip=True)
         
-        czas_emisji = ''
+        czas_emisji_str = ''
         wykonawca = ''
         tytul = ''
 
+        # Próbujemy wyodrębnić czas i resztę
+        # Format "1:00 AM - Artist - Title"
         match = re.match(r'(\d{1,2}:\d{2}\s(?:AM|PM))\s-\s(.*)', text_content)
         if match:
             czas_emisji_str = match.group(1).strip()
             artist_title_str = match.group(2).strip()
 
+            # Konwertuj czas na format 24h i dodaj dzisiejszą datę
             try:
                 dt_obj = datetime.strptime(czas_emisji_str, '%I:%M %p')
-                czas_emisji = dt_obj.strftime('%H:%M')
+                # Zakładamy, że to utwory z bieżącego dnia
+                dzisiejsza_data = datetime.now().strftime('%d.%m.%Y')
+                czas_emisji_pelny = f"{dzisiejsza_data} {dt_obj.strftime('%H:%M')}"
             except ValueError:
-                czas_emisji = czas_emisji_str 
-
+                czas_emisji_pelny = f"{datetime.now().strftime('%d.%m.%Y')} {czas_emisji_str}" # Fallback
+            
             if ' - ' in artist_title_str:
                 parts = artist_title_str.split(' - ', 1)
                 wykonawca = parts[0].strip()
@@ -108,60 +109,78 @@ def parsuj_ukradiolive(html_content, stacja_nazwa):
             else:
                 tytul = artist_title_str 
         else:
-            tytul = text_content 
+            # Jeśli format nie pasuje, użyj całego tekstu jako tytułu i bieżącego czasu
+            tytul = text_content
+            czas_emisji_pelny = f"{datetime.now().strftime('%d.%m.%Y')} {datetime.now().strftime('%H:%M')}"
 
-        data_emisji = datetime.now().strftime('%d.%m.%Y') # Używamy dzisiejszej daty
 
+        # Na tych stronach brakuje daty emisji, obrazków ani ID YouTube - zostawiamy puste
         lista_parsowanych_utworow.append({
             'stacja': stacja_nazwa,
+            'stacja_url': stacja_url,
             'wykonawca': wykonawca,
             'tytul': tytul,
-            'data_emisji': data_emisji,
-            'czas_emisji': czas_emisji,
-            'image_url_big': '', 
-            'youtube_link': '' 
+            'czas_emisji_pelny': czas_emisji_pelny, 
+            'image_url_big': '', # Brak na tej stronie
+            'youtube_id': '' # Brak na tej stronie
         })
     return lista_parsowanych_utworow
 
 
-def wygeneruj_rss(wszystkie_utwory, nazwa_pliku="ALL_RADIO.xml"):
+def wygeneruj_rss(wszystkie_utwory, nazwa_pliku="radio_playlist.xml"):
     """Generuje plik RSS 2.0 z danych utworów ze wszystkich stacji."""
     root = ET.Element('rss', version='2.0')
     channel = ET.SubElement(root, 'channel')
-    ET.SubElement(channel, 'title').text = 'Agregowana Playlista Radiowa'
-    ET.SubElement(channel, 'link').text = 'https://github.com/lily-monroe/RSS' 
+    ET.SubElement(channel, 'title').text = 'ALL RADIO' # Zgodnie z wymaganiem
+    ET.SubElement(channel, 'link').text = 'https://github.com/lily-monroe/RSS' # Link do Twojego repo
     ET.SubElement(channel, 'description').text = 'Playlista z wielu stacji radiowych'
+
+    # Opcjonalnie: sortowanie utworów przed dodaniem do RSS
+    # Jeśli chcesz najnowsze na górze, upewnij się, że czas_emisji_pelny jest parsowalny
+    # i użyj funkcji datetime do sortowania. 
+    # np. posortowane_utwory = sorted(wszystkie_utwory, key=lambda x: datetime.strptime(x['czas_emisji_pelny'], '%d.%m.%Y %H:%M'), reverse=True)
+    # Dla prostoty na razie dodajemy w kolejności zbierania
 
     for utwor in wszystkie_utwory:
         item = ET.SubElement(channel, 'item')
         
-        item_title = f"[{utwor['stacja']}] {utwor['wykonawca']} - {utwor['tytul']}"
-        if utwor['data_emisji'] and utwor['czas_emisji']:
-             item_title += f" | {utwor['data_emisji']} {utwor['czas_emisji']}"
+        # Element <title>
+        item_title = f"[{utwor['stacja']}] {utwor['wykonawca']} - {utwor['tytul']} | {utwor['czas_emisji_pelny']}"
+        ET.SubElement(item, 'title').text = item_title
         
+        # Element <description>
         item_description_parts = []
         if utwor['image_url_big']:
             item_description_parts.append(f'<img src="{utwor["image_url_big"]}"><br><br>')
         
         item_description_parts.append(f'<b>{utwor["wykonawca"]} - {utwor["tytul"]}</b><br>')
+        item_description_parts.append(f'{utwor["czas_emisji_pelny"]} | ')
         
-        if utwor['data_emisji'] and utwor['czas_emisji']:
-            item_description_parts.append(f'{utwor["data_emisji"]} {utwor["czas_emisji"]} | ')
-        
-        encoded_artist = requests.utils.quote(utwor['wykonawca'])
-        encoded_album = requests.utils.quote(utwor['tytul'])
+        # Link do COVER - kodujemy wykonawcę i tytuł
+        encoded_artist = quote_plus(utwor['wykonawca'])
+        encoded_album = quote_plus(utwor['tytul']) # Album traktujemy jak tytuł utworu dla covers.musichoarders.xyz
         cover_link = f"https://covers.musichoarders.xyz/?artist={encoded_artist}&album={encoded_album}&country=us&sources=amazonmusic"
         item_description_parts.append(f'<a href="{cover_link}">COVER</a>')
 
-        if utwor['youtube_link']:
-            item_description_parts.append(f' | <a href="{utwor["youtube_link"]}">YOUTUBE</a>')
+        # Link do YOUTUBE - używamy pobranego {%1} (youtube_id)
+        if utwor['youtube_id']:
+            youtube_full_link = f'http://youtube.com/watch?v={utwor["youtube_id"]}'
+            item_description_parts.append(f' | <a href="{youtube_full_link}">YOUTUBE</a>')
         
         item_description = "".join(item_description_parts)
-
-        ET.SubElement(item, 'title').text = item_title
         ET.SubElement(item, 'description').text = item_description
+
+        # Element <link> - adres playlisty stacji
+        ET.SubElement(item, 'link').text = utwor['stacja_url']
+        
+        # pubDate (opcjonalnie: konwersja czasu emisji na format RFC 822)
+        # Dla prostoty, używamy aktualnej daty generowania, lub jeśli czas jest pewny, można parsować
+        # try:
+        #     dt_pub = datetime.strptime(utwor['czas_emisji_pelny'], '%d.%m.%Y %H:%M')
+        #     ET.SubElement(item, 'pubDate').text = dt_pub.strftime('%a, %d %b %Y %H:%M:%S %z') # RFC 822 format
+        # except ValueError:
         ET.SubElement(item, 'pubDate').text = datetime.now().strftime('%a, %d %b %Y %H:%M:%S %z')
-        ET.SubElement(item, 'link').text = 'https://github.com/lily-monroe/RSS' 
+
 
     tree = ET.ElementTree(root)
     tree.write(nazwa_pliku, encoding='UTF-8', xml_declaration=True)
@@ -170,19 +189,19 @@ def wygeneruj_rss(wszystkie_utwory, nazwa_pliku="ALL_RADIO.xml"):
 
 if __name__ == "__main__":
     stacje_radiowe = {
-        'Radio 357': 'https://myradioonline.pl/radio-357/',
-        'ChilliZET': 'https://myradioonline.pl/chillizet/',
-        'Radio Nowy Świat': 'https://myradioonline.pl/radio-nowy-swiat/',
+        'Radio 357': 'https://myradioonline.pl/radio-357/playlista',
+        'ChilliZET': 'https://myradioonline.pl/chillizet/playlista',
+        'Radio Nowy Świat': 'https://myradioonline.pl/radio-nowy-swiat/playlista',
         'RMF FM': 'https://myradioonline.pl/rmf-fm/playlista',
-        'RMF MAXXX': 'https://myradioonline.pl/rmf-maxxx/',
-        'Radio ZET': 'https://myradioonline.pl/radio-zet/',
-        'PR Czwórka': 'https://myradioonline.pl/polskie-radio-czworka/',
-        'PR Trójka': 'https://myradioonline.pl/polskie-radio-trojka/',
-        'Radio Eska': 'https://myradioonline.pl/radio-eska/',
-        'BBC Radio 1': 'https://ukradiolive.com/bbc-radio-1/',
-        'BBC Radio 2': 'https://ukradiolive.com/bbc-radio-2/',
-        'Virgin Radio UK': 'https://ukradiolive.com/virgin-radio-uk/',
-        'Heart London': 'https://ukradiolive.com/heart-london/'
+        'RMF MAXXX': 'https://myradioonline.pl/rmf-maxxx/playlista',
+        'Radio ZET': 'https://myradioonline.pl/radio-zet/playlista',
+        'PR Czwórka': 'https://myradioonline.pl/polskie-radio-czworka/playlista',
+        'PR Trójka': 'https://myradioonline.pl/polskie-radio-trojka/playlista',
+        'Radio Eska': 'https://myradioonline.pl/radio-eska/playlista',
+        'BBC Radio 1': 'https://ukradiolive.com/bbc-radio-1/playlist',
+        'BBC Radio 2': 'https://ukradiolive.com/bbc-radio-2/playlist',
+        'Virgin Radio UK': 'https://ukradiolive.com/virgin-radio-uk/playlist',
+        'Heart London': 'https://ukradiolive.com/heart-london/playlist'
     }
 
     wszystkie_parsowane_utwory = []
@@ -192,17 +211,39 @@ if __name__ == "__main__":
         html_content = pobierz_strone(url_stacji)
         if html_content:
             if "myradioonline.pl" in url_stacji:
-                parsowane_utwory = parsuj_myradioonline(html_content, nazwa_stacji)
+                parsowane_utwory = parsuj_myradioonline(html_content, nazwa_stacji, url_stacji)
                 wszystkie_parsowane_utwory.extend(parsowane_utwory)
             elif "ukradiolive.com" in url_stacji:
-                parsowane_utwory = parsuj_ukradiolive(html_content, nazwa_stacji)
-                wszystkie_parsowane_utwory.extend(parsowane_utwory)
+                parsowane_utwory = parsuj_ukradiolive(html_content, nazwa_stacji, url_stacji)
+                wszystkie_parsowane_utwory.extend(parsowane_utwory) # Poprawiona literówka
             else:
                 print(f"Brak zdefiniowanej funkcji parsowania dla {url_stacji}")
         else:
             print(f"Nie udało się pobrać treści dla {url_stacji}")
 
     if wszystkie_parsowane_utwory:
-        wygeneruj_rss(wszystkie_parsowane_utwory, nazwa_pliku="ALL_RADIO.xml") # Nazwa pliku XML
+        # Sortowanie utworów od najnowszych
+        try:
+            # Tworzymy słownik do przechowywania dat i czasów dla sortowania
+            # Strony ukradiolive.com często nie podają roku, dlatego sortowanie może być niedokładne dla dat z różnych dni/lat
+            # Jeśli utwory z ukradiolive.com mają tylko godzinę, traktujemy je jako z dzisiejszej daty.
+            # Konwersja do datetime jest kluczowa dla poprawnego sortowania
+            
+            # Spróbuj parsować daty i godziny, a te które się nie uda, zostaw na końcu lub na początku
+            def get_sort_key(item):
+                try:
+                    # Spróbuj parsować datę w formacie dd.mm.YYYY HH:MM
+                    return datetime.strptime(item['czas_emisji_pelny'], '%d.%m.%Y %H:%M')
+                except ValueError:
+                    # Jeśli format się nie zgadza (np. dla ukradiolive), zwróć minimalną datę, 
+                    # aby te utwory były na końcu listy po posortowaniu malejąco
+                    return datetime.min 
+
+            posortowane_utwory = sorted(wszystkie_parsowane_utwory, key=get_sort_key, reverse=True)
+            wygeneruj_rss(posortowane_utwory, nazwa_pliku="radio_playlist.xml")
+        except Exception as e:
+            print(f"Błąd podczas sortowania utworów: {e}. Generuję RSS bez sortowania.")
+            wygeneruj_rss(wszystkie_parsowane_utwory, nazwa_pliku="radio_playlist.xml")
+            
     else:
         print("Nie pobrano żadnych utworów z żadnej stacji.")
